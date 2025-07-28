0c98612b534abe859f08639527a0cea74545e66c
saikumar <saikumar9441444@gmail.com>
2025-07-28 22:18:37 +0530

    Fix: connectDB path and added connect call

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,16 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncuserUpdation } from "@/config/inngest";
+import { inngest, syncUserCreation, syncUserDeletion, syncuserUpdation } from "../../../config/inngest";
+import connectDB from "../../../lib/connectDB";
 
-// Create an API that serves zero functions
+// Connect to DB before running the functions
+await connectDB(); // <-- Add this line (if top-level await is allowed in your config)
+
+// Create an API that serves functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-   syncUserCreation,
-   syncuserUpdation,
-   syncUserDeletion
+    syncUserCreation,
+    syncuserUpdation,
+    syncUserDeletion,
   ],
 });
