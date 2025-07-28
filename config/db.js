d95e4bf17023ca85e8d776450232c1a5b4141bcc
saikumar <saikumar9441444@gmail.com>
2025-07-28 11:34:20 +0530

    First Commit

---
+++ config/db.js
@@ -0,0 +1,31 @@
+import mongoose, { connect } from "mongoose";
+
+let cached = global.mongoose
+
+if (!cached){
+    cached = global.mongoose = { conn: null,Promise: null}
+}
+
+async function connectDb() {
+
+    if (cached.com) {
+        return cached.conn
+    }
+
+    if (!cached.promise){
+        const opts ={
+            bufferCommands:false
+        }
+
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then(mongoose =>{
+            return mongoose
+        })
+
+    }
+
+    cached.conn = await cached.promise
+    return cached.conn
+
+}
+
+export default connectDB
