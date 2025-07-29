33ae5be28db995c9506edc3137023b2ba5335efb
saikumar <saikumar9441444@gmail.com>
2025-07-29 13:38:09 +0530

    second commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,16 +1,12 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncuserUpdation } from "../../../config/inngest";
-import connectDB from "../../../lib/connectDB";
+import { inngest, syncUserCreation, syncUserDeletion, syncuserUpdation } from "@/config/inngest";
 
-// Connect to DB before running the functions
-await connectDB(); // <-- Add this line (if top-level await is allowed in your config)
-
-// Create an API that serves functions
+// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
     syncuserUpdation,
-    syncUserDeletion,
+    syncUserDeletion
   ],
 });
