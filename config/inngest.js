d6fa0f03000e543db5c72134eaa637101f261a39
saikumar <saikumar9441444@gmail.com>
2025-07-30 16:14:07 +0530

    email Commit

--- config/inngest.js
+++ config/inngest.js
@@ -15,7 +15,7 @@ export const syncUserCreation = inngest.createFunction(
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
             _id: id,
-            email: email_addressess[0].email_address,
+            email: email_addresses[0].email_address,
             name: first_name + '' + last_name,
             image_url: image_url
         }
@@ -34,7 +34,7 @@ export const syncuserUpdation = inngest.createFunction(
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const userData = {
             _id: id,
-            email: email_addressess[0].email_address,
+            email: email_addresses[0].email_address,
             name: first_name + '' + last_name,
             image_url: image_url
         }
