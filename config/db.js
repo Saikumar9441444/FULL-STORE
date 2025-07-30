f00155e664a6ce6fc049dd02af3eccc0dcb87b89
saikumar <saikumar9441444@gmail.com>
2025-07-30 15:18:26 +0530

    best Commit

--- config/db.js
+++ config/db.js
@@ -1,23 +1,23 @@
-import mongoose, { connect } from "mongoose";
+import mongoose from "mongoose";
 
 let cached = global.mongoose
 
-if (!cached){
-    cached = global.mongoose = { conn: null,Promise: null}
+if (!cached) {
+    cached = global.mongoose = { conn: null, Promise: null }
 }
 
-async function connectDb() {
+async function connectDB() {
 
     if (cached.com) {
         return cached.conn
     }
 
-    if (!cached.promise){
-        const opts ={
-            bufferCommands:false
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false
         }
 
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).then(mongoose =>{
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then(mongoose => {
             return mongoose
         })
 
